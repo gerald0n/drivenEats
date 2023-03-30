c1617bf53c6039e2426c3a09c49f2627a87a80ea
Geraldo Gomes <contato.gneto@gmail.com>
2023-03-30 11:56:13 -0300

    adição de funcionalidade FINALIZADA: check e uncheck caixas separadas por categoria

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,42 +1,33 @@
 const listFoods = document.querySelectorAll('.food')
+const listDrinks = document.querySelectorAll('.drink')
+const listDesserts = document.querySelectorAll('.dessert')
 
 listFoods.forEach(food => {
     food.addEventListener('click', () => {
-        
-        const foods = [];
-        for (const food in listFoods) {
-            if (Object.hasOwnProperty.call(listFoods, food)) {
-                if(listFoods[food].classList.value.includes('outraclasse')) {
-                    const [a, b] = listFoods[food].classList.value.split(' ')
-                    console.log(listFoods[food]) //identificar se alguma div está utilizando a classe 
-                    // console.log(b)
-                }
-                foods.push(listFoods[food].classList.value)
-            }
-        }
+        check(food)
+        listFoods.forEach(foodClass => {
+            uncheck(foodClass, food)
+        })
     })
-git 
+})
 
+listDrinks.forEach(drink => {
+    drink.addEventListener('click', () => {
+        check(drink)
+        listDrinks.forEach(drinkClass => {
+            uncheck(drinkClass, drink)
+        })
+    })
+})
 
-// console.log(foods)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+listDesserts.forEach(dessert => {
+    dessert.addEventListener('click', () => {
+        check(dessert)
+        listDesserts.forEach(dessertClass => {
+            uncheck(dessertClass, dessert)
+        })
+    })
+})
 
 
 
@@ -45,44 +36,25 @@ git
 
 
 
+const check = divClicked => {
+    divClicked.classList.add('b-enabled')
+    divClicked
+        .querySelector('#price-and-check div')
+        .classList.remove('i-disabled')
+}
+const uncheck = (divCurrent, divClicked) => {
+    if (
+        divCurrent.classList.value.includes('b-enabled') &&
+        divCurrent != divClicked
+    ) {
+        divCurrent.classList.remove('b-enabled')
+        divCurrent
+            .querySelector('#price-and-check div')
+            .classList.add('i-disabled')
 
-
-// arrFood.forEach(food => {
-
-//     food.addEventListener('click', () => {
-        
-//         const listClasses = []
-
-//         if (!food.classList.contains('b-enabled')) {
-//             food.classList.add('b-enabled')
-//             food.querySelector('#price-and-check div').classList.remove(
-//                 'i-disabled'
-//             )
-//         }
-//     })
-// })
-
-// arrFood.forEach(food => {
-//     // quando clicar no quadro de comida
-//     food.addEventListener('click', () => {
-
-//         const newArr = []
-//         arrFood.forEach(food => {
-//             food.classList.forEach(food => {
-//                 newArr.push(food) // cria um novo arr listando todas as classes
-//             })
-//         })
-
-//         if (!newArr.includes('b-enabled')) { //verifica se NÃO há a classe b-enabled, não havendo:
-//             food.classList.add('b-enabled') //adiciona a classe b-enabled com borda
-//             food.querySelector('#price-and-check div').classList.remove('i-disabled') //remove a classe i-disabled e habilita o icone
-//             }
-//         else {
-//             // TODO
-//         }
-
-//             })
-// })
-
-// // Ao clicar no food, deve-se verificar se há algum marcado; se houver, é preciso desmarcá-lo e marcar o outro.
-// // Se não houver nenhum marcado, marcar.
+        divClicked.classList.add('b-enabled')
+        divClicked
+            .querySelector('#price-and-check div')
+            .classList.remove('i-disabled')
+    }
+}
